9f6d701d2899a3f3fb59090f15d2b4c8129a55cb
Felipe Henrique <felipehfsouza@gmail.com>
2022-11-25 07:37:53 -0300

    feat: added mirage in prod

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import App from "./App";
 import "./index.css";
 import { makeServer } from "./services/service";
 
-if (process.env.NODE_ENV === "development") {
-  makeServer({ environment: "development" });
-}
+makeServer({ environment: "development" });
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
